0fbd9901510d6aa2cb6091fc2ecc737941a7c38f
NGHIA Nguyen Duc Dai <nghia.nguyenduc@eightcap.com>
2022-08-08 17:02:13 +0700

    Demo DataTable

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,12 @@ import H1 from 'dmf_ui/typography/H1';
 import { TodosView as UserTodosView } from 'dmf_user/todos';
 import TodoList from './features/todos/components/TodoList';
 import DynamicForm, { FormInput } from 'dmf_ui/dynamicForm';
+import DataTable, {
+  Icolumn,
+  Irow,
+  ItableStyle,
+  renderFunction,
+} from 'dmf_ui/DataTable';
 
 export const App = () => {
   const formData: (FormInput | FormInput[])[] = [
@@ -209,8 +215,154 @@ export const App = () => {
               defaultValues={defaultValue}
             />
           </div>
+          <div tw="m-2">
+            <DemoTable />
+          </div>
         </div>
       </NotAuthLayout>
+    </>
+  );
+};
+
+const DemoTable = () => {
+  const columns: Icolumn[] = [
+    {
+      field: 'created_at',
+      use: 'Action',
+      // use_in_search:false
+    },
+    {
+      field: 'front_end_position.name',
+      use: 'Position',
+      useInSearch: true,
+    },
+    {
+      // use_in_display: false,
+      field: 'name',
+      use: 'Name',
+    },
+
+    {
+      field: 'country_id',
+      use: 'Country',
+
+      //Will not be displayed in the table
+      useInDisplay: false,
+    },
+    {
+      field: 'club_id',
+      use: 'Club Details',
+      //would not be exported as a CSV column
+      useInDisplay: true,
+    },
+  ];
+
+  const rowA: Irow[] = [
+    {
+      id: 1,
+      name: 'Sadio Mane',
+      country_id: 3,
+      club_id: 2,
+      position_id: 1,
+      shirt_number: '10',
+      created_by: 2,
+      deleted_at: null,
+      created_at: '12/12/12 15:00:00',
+      updated_at: '12/12/12 15:00:00',
+      is_defender: false,
+      is_midfielder: false,
+      is_forward: true,
+      is_goalkeeper: false,
+      front_end_position: {
+        name: 'attach',
+        id: 2,
+      },
+    },
+    {
+      id: 2,
+      name: 'Mohammed Sala',
+      country_id: 3,
+      club_id: 2,
+      position_id: 1,
+      shirt_number: '11',
+      created_by: 2,
+      deleted_at: null,
+      created_at: '12/12/12 15:00:00',
+      updated_at: '12/12/12 15:00:00',
+      is_defender: false,
+      is_midfielder: false,
+      is_forward: true,
+      is_goalkeeper: false,
+      front_end_position: {
+        name: 'Forward',
+        id: 4,
+      },
+    },
+    {
+      id: 3,
+      name: 'Robertor Fermino',
+      country_id: 3,
+      club_id: 2,
+      position_id: 1,
+      shirt_number: '8',
+      created_by: 2,
+      deleted_at: null,
+      created_at: '12/12/12 15:00:00',
+      updated_at: '12/12/12 15:00:00',
+      is_defender: false,
+      is_midfielder: false,
+      is_forward: true,
+      is_goalkeeper: false,
+      front_end_position: {
+        name: 'Defence',
+        id: 9,
+      },
+    },
+  ];
+
+  const rows = rowA.concat(rowA).concat(rowA);
+
+  const rowRender: renderFunction = (
+    row: Irow,
+    column: Icolumn,
+    display_value: any
+  ) => {
+    if (column.field === 'created_at') {
+      return <button className="border p-2">See button</button>;
+    }
+
+    if (column.field === 'name') {
+      return <b>{display_value}</b>;
+    }
+
+    return display_value;
+  };
+  const style: ItableStyle = {
+    baseBgColor: 'primary',
+    top: {
+      elements: {
+        search: {
+          text: 'Search In Table',
+          placeHolder: 'Mane ...',
+        },
+        bulkSelect: {
+          button: 'Bulk Select',
+          main: 'bulk-select',
+        },
+      },
+    },
+  };
+  return (
+    <>
+      <DataTable
+        columns={columns}
+        rows={rows}
+        perPage={2}
+        tableHeader="Test Table"
+        rowRender={rowRender}
+        styling={style}
+        bulkSelectOptions={['Delete']}
+      />
     </>
   );
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
 import('./Root');
+/*
+type PropEventSource<Type> = {
+  on<Key extends string & keyof Type>(
+    eventName: `${Key}Changed`,
+    callback: (newValue: Type[Key]) => void
+  ): void;
+};
+
+declare function makeWatchedObject<Type>(
+  obj: Type
+): Type & PropEventSource<Type>;
+
+const person = makeWatchedObject({
+  firstName: 'nghia',
+  lastName: 'Nguyen',
+  age: 38,
+});
+
+person.on('firstNameChanged', (val) => {
+  console.log(val);
+});
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+type P = keyof Point;
+const p: P = 'x';
+
+function f() {
+  return {
+    x: 12,
+    y: 13,
+    z: '23',
+  };
+}
+
+type F = ReturnType<typeof f>;
+const f_const: F = {
+  x: 1,
+  y: 20,
+  z: 'Z val',
+};
+
+const MyArray = [
+  { name: 'Alice', age: 15 },
+  { name: 'Bob', age: 23 },
+  { name: 'Eve', age: 38 },
+];
+
+type Person = Partial<typeof MyArray[0]>;
+const p1: Person = {
+  age: 13,
+//   name: '232',
+};
+*/
